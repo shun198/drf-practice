5d90f493b8056512b516767792ec3825a1d64a5b
shun198 <shunhiroseluvmri@gmail.com>
2023-01-22 20:10:06 +0900

    poetryのcacheを使用

--- project/settings.py
+++ project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "NAME": os.environ.get("MYSQL_DATABASE"),
         "USER": os.environ.get("MYSQL_USER"),
         "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
-        "HOST": os.environ.get("MYSQL_DATABASE"),
+        "HOST": "db",
         "PORT": 3306,
     }
 }
