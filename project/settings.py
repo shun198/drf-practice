1be07d3265648d17ee9947a4c75098492a08a372
shun198 <shunhiroseluvmri@gmail.com>
2023-01-22 19:55:53 +0900

    MYSQL_DATABASEを追加#

--- project/settings.py
+++ project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "NAME": os.environ.get("MYSQL_DATABASE"),
         "USER": os.environ.get("MYSQL_USER"),
         "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
-        "HOST": "db",
+        "HOST": os.environ.get("MYSQL_DATABASE"),
         "PORT": 3306,
     }
 }
