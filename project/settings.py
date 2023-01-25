96ecf9c0e3d0d53b94c62f5d5f31e798e54e499e
shun198 <shunhiroseluvmri@gmail.com>
2023-01-25 15:54:24 +0900

    👍 ALLOWED_HOSTSを変更

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
