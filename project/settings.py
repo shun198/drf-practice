ee6653f55ea16c0644b6e532c75e114da77d6609
shun198 <shunhiroseluvmri@gmail.com>
2023-01-26 08:19:54 +0900

    👍 GETHUB_WORKFLOWでDBを制御

--- project/settings.py
+++ project/settings.py
@@ -84,18 +84,31 @@ WSGI_APPLICATION = "project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        # コンテナ内の環境変数をDATABASESのパラメータに反映
-        "NAME": "django-db",
-        "USER": "django",
-        "PASSWORD": "django",
-        "HOST": "127.0.0.1",
-        "PORT": 3306,
-    }
-}
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            # コンテナ内の環境変数をDATABASESのパラメータに反映
+            "NAME": "django-db",
+            "USER": "django",
+            "PASSWORD": "django",
+            "HOST": "127.0.0.1",
+            "PORT": 3306,
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            # コンテナ内の環境変数をDATABASESのパラメータに反映
+            "NAME": "django-db",
+            "USER": "django",
+            "PASSWORD": "django",
+            "HOST": "db",
+            "PORT": 3306,
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
