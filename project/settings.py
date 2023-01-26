70ee2c4748bd5fca2eebbdb9ae08805132fbe4a2
shun198 <shunhiroseluvmri@gmail.com>
2023-01-26 10:07:47 +0900

    👍 ALLOWED_HOSTSがない時は空配列を返す

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "TEST")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = os.environ.get(("DJANGO_ALLOWED_HOSTS").split(" "),"")
+ALLOWED_HOSTS = (os.environ.get("DJANGO_ALLOWED_HOSTS").split(" "),[])
 
 
 # Application definition
