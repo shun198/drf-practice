546df8e7a5bdb801adef731680d567f7a2e37786
shun198 <shunhiroseluvmri@gmail.com>
2023-01-26 16:38:10 +0900

    👍 127.0.0.1をlocalhostに変更

--- project/settings.py
+++ project/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": os.environ.get("MYSQL_DATABASE"),
         "USER": os.environ.get("MYSQL_USER"),
         "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
-        "HOST": os.environ.get("MYSQL_HOST", "127.0.0.1"),
+        "HOST": os.environ.get("MYSQL_HOST", "localhost"),
         "PORT": os.environ.get("MYSQL_PORT", 3306),
     }
 }
