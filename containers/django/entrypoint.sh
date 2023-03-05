c565867ff293d31c301a044e34802984c420cba9
shun198 <shunhiroseluvmri@gmail.com>
2023-03-05 11:12:12 +0900

    👍 entrypoint.shをDJANGO_SETTINGS_MODULEを見るよう修正

--- containers/django/entrypoint.sh
+++ containers/django/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
 
-if [ $DEBUG = "True" ]
+if [ $DJANGO_SETTINGS_MODULE = "project.settings.dev" ]
 then
     poetry run python manage.py runserver 0.0.0.0:8000
 else
