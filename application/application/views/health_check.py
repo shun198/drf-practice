1d6d90bcfd8279d8eb3a99eaa66f07e3c8e5209a
shun198 <shunhiroseluvmri@gmail.com>
2023-03-18 17:36:04 +0900

    👍 logs.pyとget_login_ip.pyを追加

--- application/application/views/health_check.py
+++ application/application/views/health_check.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework import status
 
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def health_check(request):
     """ヘルスチェック"""
-    return JsonResponse(data={"msg": "pass"}, status=200)
+    return JsonResponse(data={"msg": "pass"}, status=status.HTTP_200_OK)
