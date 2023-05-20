0bb5d5425f4f198978d177f97df303915f9e3830
shun198 <shunhiroseluvmri@gmail.com>
2023-05-21 08:35:23 +0900

    👍 add user tests

---
+++ application/application/tests/views/test_user.py
@@ -0,0 +1,46 @@
+import pytest
+
+from rest_framework import status
+
+from application.tests.common_method import login
+
+
+@pytest.fixture
+def get_user_url():
+    return "/api/users/"
+
+
+@pytest.fixture
+def get_user_details_url(id):
+    return f"/api/users/{id}/"
+
+
+@pytest.mark.django_db
+def test_management_user_can_list_users(client, login_management, get_user_url):
+    """管理者ユーザでユーザの一覧を表示できるテスト"""
+    login(client, login_management)
+    response = client.get(get_user_url, format="json")
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_general_user_can_list_users(client, login_management, get_user_url):
+    """一般ユーザでユーザの一覧を表示できるテスト"""
+    login(client, login_management)
+    response = client.get(get_user_url, format="json")
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_sales_user_can_list_users(client, login_part_time, get_user_url):
+    """アルバイトユーザでユーザの一覧を表示できるテスト"""
+    login(client, login_part_time)
+    response = client.get(get_user_url, format="json")
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_user_cannot_list_users_without_login(client, get_user_url):
+    """ログインなしでユーザの一覧を表示できないテスト"""
+    response = client.get(get_user_url, format="json")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
