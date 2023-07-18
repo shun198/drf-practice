52c1f44297f07593edc1257e9e5e05a47c2c2196
s.hirose <shunichi_hirose@pj100.biz>
2023-07-18 16:57:25 +0900

    👍 change base.py

--- application/project/settings/base.py
+++ application/project/settings/base.py
@@ -171,7 +171,7 @@ BROKER_TRANSPORT_OPTIONS = {
 # 自身以外のオリジンのHTTPリクエスト内にクッキーを含めることを許可する
 CORS_ALLOW_CREDENTIALS = True
 # アクセスを許可したいURL（アクセス元）を追加
-CORS_ALLOWED_ORIGINS = os.environ.get("TRUSTED_ORIGINS").split(" ")
+CORS_ALLOWED_ORIGINS = django_settings.TRUSTED_ORIGINS.split(" ")
 # プリフライト(事前リクエスト)の設定
 # 30分だけ許可
 CORS_PREFLIGHT_MAX_AGE = 60 * 30
