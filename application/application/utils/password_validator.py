ad1fde317a18d9d8452933acd285b0a408f8cb79
shun198 <shunhiroseluvmri@gmail.com>
2023-08-15 15:48:58 +0900

    👍 change validator

--- application/application/utils/password_validator.py
+++ application/application/utils/password_validator.py
@@ -8,6 +8,4 @@ class PasswordValidator:
         REX = r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*_\-+=`|(){}\[\]:;\"\'<>,.?/]).{8,64}"
         result = re.fullmatch(REX, password)
         if not result:
-            raise ValidationError(
-                "パスワードは次の4種類全てを含めた上で8文字以上、64文字以下にしてください。半角英数字・記号(!@#$%+-=)"
-            )
+            raise ValidationError("英大小文字、数字、または特殊文字が含まれていません")
